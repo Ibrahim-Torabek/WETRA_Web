03404d6ab6014c0014a2fe644e7d7198204916cc
Ibrahim Torabek <wusiman.yibulayin01@stclairconnect.ca>
2022-03-06 21:09:37 -0500

    User Table Updated #4    * deleted name field    * added first and last name fields    * added some other fields.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->integer('group_id')->nullable();
+            $table->string('image_url')->nullable();
+            $table->string('job_title')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
+            $table->timestamp('register_date')->nullable()->default(time());
+            $table->tinyInteger('status_id');
+            $table->boolean('is_admin')->nullable()->default(false);
+
         });
     }
 
