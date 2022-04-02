c8b4cdf06dd37c304f4de10b1d797a95090e7b5f
Ibrahim Torabek <wusiman.yibulayin01@stclairconnect.ca>
2022-04-02 12:50:13 -0400

    Modified Group Table    * Counted ID from 10000    * Seeded with some gorups.

--- database/migrations/2022_04_01_195957_create_groups_table.php
+++ database/migrations/2022_04_01_195957_create_groups_table.php
@@ -14,7 +14,7 @@ class CreateGroupsTable extends Migration
     public function up()
     {
         Schema::create('groups', function (Blueprint $table) {
-            $table->id();
+            $table->id()->startingValue(10000);
 
             $table->string("name");
             $table->integer("admin")->nullable();
