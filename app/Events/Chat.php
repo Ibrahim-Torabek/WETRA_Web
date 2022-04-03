815c02ddca1e26f0ea701aa86bf3120cd3b98d4f
Ibrahim Torabek <wusiman.yibulayin01@stclairconnect.ca>
2022-04-03 16:37:58 -0400

    Pusher Completed    * User can receive new message without reloaidng the page.    * Issue Last massage covered by chat box #33 completed

---
+++ app/Events/Chat.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Chat implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($userName, $message)
+    {
+        $this->user = $userName;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('chat');
+    }
+
+    public function broadcastAs()
+    {
+        return 'message';
+    }
+}
