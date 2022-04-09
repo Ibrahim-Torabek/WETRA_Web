104db95c53dd301473bb1f216c5de07c80cdd698
Elena Polyakova <elena.polyakova98@stclairconnect.ca>
2022-04-09 01:40:04 -0400

    #13 Settings UI completed

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
 <div class="container pt-5">
-  <div class="row justify-content-left">
+  <div class="col">        
     <!-- Notifications card -->
-    <div class="row justify-content-center">
-      <div class="col pt-4">
-       <h4 class="text-center pb-2">{{ __('Settings') }}</h4>
+    <div class="row pt-4">
+      <!-- <div class="col pt-4"> -->
+      <h4 class="text-center pb-2">{{ __('Settings') }}</h4>
+      <div class="col">
         <div class="card">
           <div class="card-header text-left">
             <h5>{{ __('Notifications') }}</h5>
@@ -21,7 +22,7 @@
                     <input type="checkbox" class="custom-control-input" id="fileUploadedCheck">
                     <label class="custom-control-label pb-2" for="fileUploadedCheck">New file uploaded</label>
                   </div>
-                  <div class="custom-control custom-checkbox border-bottom">
+                  <div class="custom-control custom-checkbox border-bottom mt-2">
                     <input type="checkbox" class="custom-control-input" id="messageReceivedCheck">
                     <label class="custom-control-label pb-2" for="messageReceivedCheck">New message received</label>
                   </div>
@@ -31,7 +32,7 @@
                     <input type="checkbox" class="custom-control-input" id="passwordChangedCheck">
                     <label class="custom-control-label pb-2" for="passwordChangedCheck">Password changed</label>
                   </div>
-                  <div class="custom-control custom-checkbox border-bottom">
+                  <div class="custom-control custom-checkbox border-bottom mt-2">
                     <input type="checkbox" class="custom-control-input" id="personalInformationCheck">
                     <label class="custom-control-label pb-2" for="personalInformationCheck">Personal information changed</label>
                   </div>
@@ -40,8 +41,8 @@
               <!-- Button to Save Changes to the Notifications -->
               <div class="row">
                 <div class="mt-4 text-center">
-                  <button type="submit" class="btn btn-primary">
-                      {{ __('Save Changes') }}
+                  <button type="submit" class="btn btn-primary btn-lg">
+                      {{ __('Save Notifications') }}
                   </button>
                 </div>
               </div>
@@ -50,63 +51,63 @@
         </div>
       </div>
     </div>
-
+    
     <!-- Change password card -->
-    <div class="col">
-      <div class="card">
-        <div class="card-header text-left">
-          <h5 class="mb-0">{{ __('Change Password') }}</h5>
-        </div>
-        <div class="card-body">       
-          <form method="POST" action="">
-            @csrf
-            <!-- Current password field                      -->
-            <div class="row g-3 align-items-center">
-              <div class="col-auto">
-                <label for="inputCurrentPassword" class="col-form-label">Current password</label>
+    <div class="row pt-4">
+      <div class="col">
+        <div class="card">
+          <div class="card-header text-left">
+            <h5 class="mb-0">{{ __('Change Password') }}</h5>
+          </div>
+          <div class="card-body">       
+            <form method="POST" action="">
+              @csrf
+              <!-- Current password field -->
+              <div class="row align-items-center">
+                <div class="col-md-2">
+                  <label for="inputCurrentPassword" class="col-form-label">Current password</label>
+                </div>
+                <div class="col">
+                  <input type="password" id="inputCurrentPassword" class="form-control" aria-label="Type your current password">
+                </div>          
               </div>
-              <div class="col-auto">
-                <input type="password" id="inputCurrentPassword" class="form-control" aria-describedby="currentPasswordHelpInline">
-              </div>
-              <div class="col-auto">
-                <span id="currentPasswordHelpInline" class="form-text">
-                  Type your current password
-                </span>
-              </div>
-            </div>
 
-            <!-- New password field -->
-            <div class="row g-1 align-items-center mt-1">
-              <div class="col-auto">
-                <label for="inputNewPassword" class="col-form-label">New password</label>
+              <!-- New password field -->
+              <div class="row align-items-center mt-3">
+                <div class="col-md-2">
+                  <label for="inputNewPassword" class="col-form-label">New password</label>
+                </div>
+                <div class="col">
+                  <input type="password" id="inputNewPassword" class="form-control" aria-describedby="newPasswordHelpBlock">
+                  <div id="newPasswordHelpBlock" class="form-text">
+                    Must be 8-20 characters long and contain only letters and numbers.
+                  </div>
+                </div>
               </div>
-              <div class="col-auto">
-                <input type="password" id="inputNewPassword" class="form-control" aria-describedby="newPasswordHelpInline">
-              </div>
-              <div class="col-auto">
-                <span id="newPasswordHelpInline" class="form-text">
-                Must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-                </span>
-              </div>
-            </div>
 
-            <!-- Confirm password field -->
-            <div class="row g-3 align-items-center mt-1">
-              <div class="col-auto">
-                <label for="confirmNewPassword" class="col-form-label">New password</label>
+              <!-- Confirm password field -->
+              <div class="row align-items-center mt-3">
+                <div class="col-md-2">
+                  <label for="confirmNewPassword" class="col-form-label">Confirm password</label>
+                </div>
+                <div class="col">
+                  <input type="password" id="confirmNewPassword" class="form-control" aria-label="Confirm your new password">
+                </div>
+                
               </div>
-              <div class="col-auto">
-                <input type="password" id="confirmNewPassword" class="form-control" aria-describedby="confirmPasswordHelpInline">
+
+              <!-- Button to Save password change -->
+              <div class="row text-center">
+                <div class="mt-4">
+                  <button type="submit" class="btn btn-primary btn-lg">
+                      {{ __('Update Password') }}
+                  </button>
+                </div>
               </div>
-              <div class="col-auto">
-                <span id="confirmPasswordHelpInline" class="form-text">
-                Type your new password to confirm.
-                </span>
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </div>     
 </div>
